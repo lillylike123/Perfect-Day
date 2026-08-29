62bb22d72c461237da3d73793d67c9ffd334adca
ChibiMooshroom <bread.cool.stuff@gmail.com>
2026-08-29 17:35:41 -0400

    hhh

--- cindy.js
+++ cindy.js
@@ -6,21 +6,29 @@ let speedX = 3;
 let speedY = 3;
 
 function animate() {
-  const imgWidth = img.clientWidth;
-  const imgHeight = img.clientHeight;
+  const imgWidth = img.clientWidth || 80;
+  const imgHeight = img.clientHeight || 80;
   const screenWidth = window.innerWidth;
   const screenHeight = window.innerHeight;
 
-  if (x + imgWidth >= screenWidth || x <= 0) {
-    speedX = -speedX;
-  }
-
-  if (y + imgHeight >= screenHeight || y <= 0) {
-    speedY = -speedY;
-  }
-
   x += speedX;
   y += speedY;
+
+  if (x + imgWidth >= screenWidth) {
+    speedX = -Math.abs(speedX);
+    x = screenWidth - imgWidth;
+  } else if (x <= 0) {
+    speedX = Math.abs(speedX);
+    x = 0;
+  }
+
+  if (y + imgHeight >= screenHeight) {
+    speedY = -Math.abs(speedY);
+    y = screenHeight - imgHeight;
+  } else if (y <= 0) {
+    speedY = Math.abs(speedY);
+    y = 0;
+  }
 
   img.style.left = x + 'px';
   img.style.top = y + 'px';
@@ -28,6 +36,8 @@ function animate() {
   requestAnimationFrame(animate);
 }
 
-img.onload = () => {
+if (img.complete) {
   animate();
-};
+} else {
+  img.onload = animate;
+}
